00e4379d63be218305df2f733fde057a52b8c456
Robert Chow <rchow@thoughtworks.com>
2024-07-09 21:36:19 +0100

    RC - remove skipped christmas tree test and fixed for proper

--- src/christmas-tree/solutions/christmas-tree.spec.ts
+++ src/christmas-tree/solutions/christmas-tree.spec.ts
@@ -1,7 +1,26 @@
 import { describe, it, expect } from 'vitest'
 import { ChristmasTree } from './christmas-tree'
 
-describe.skip('ChristmasTree', () => {
+describe('ChristmasTree', () => {
+  it('renders a tree with a single row when 1 is passed in', () => {
+    const christmasTree = new ChristmasTree()
+    const actual = christmasTree.render(1)
+
+    expect(actual).toBe(`*
+|
+`)
+  })
+
+  it('renders a tree with two rows when 2 is passed in', () => {
+    const christmasTree = new ChristmasTree()
+    const actual = christmasTree.render(2)
+
+    expect(actual).toBe(` *
+***
+ |
+`)
+  })
+
   it('should render a tree with 3 rows', () => {
     const christmasTree = new ChristmasTree()
 
--- src/christmas-tree/solutions/christmas-tree.ts
+++ src/christmas-tree/solutions/christmas-tree.ts
@@ -1,41 +1,13 @@
 export class ChristmasTree {
   render(rows: number): string {
-    let maxStarsPerRow = rows + rows - 1
-
-    const treeArr: string[] = []
-
-    for (let i = 0; i < rows; i++) {
-      const currentStarsInRow = i + i + 1
-      let row = ''
-      for (let j = 0; j < currentStarsInRow; j++) {
-        row += '*'
-      }
-      treeArr.push(row)
+    var tree = ''
+    for (var i = 0; i < rows; ++i) {
+      const spacer = ' '.repeat(rows - 1 - i)
+      const leaves = '*'.repeat(i * 2 + 1)
+      tree += spacer + leaves + `\n`
     }
 
-    const finalTree = treeArr
-      .map((x, i) => {
-        const paddedSpaces = Math.floor(maxStarsPerRow / 2) + i * -1
-        return x.padStart(paddedSpaces).padEnd(paddedSpaces)
-      })
-      .join('\n')
-
-    return this.addTrunk(maxStarsPerRow, finalTree + '\n')
-  }
-
-  private addTrunk(maxStarsPerRow: number, tree: string) {
-    let lastRow = ''
-    const half = Math.floor(maxStarsPerRow / 2)
-    for (let i = 0; i < maxStarsPerRow; i++) {
-      const isHalf = i === half
-      if (isHalf) {
-        lastRow += '|'
-      } else {
-        lastRow += ' '
-      }
-    }
-    tree += lastRow + '\n'
-
-    return tree
+    const trunk = ' '.repeat(rows - 1) + '|' + `\n`
+    return tree + trunk
   }
 }
